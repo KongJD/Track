9eed341a542e3508e578f7de6af8e30d20401710
jindong <jindong123@qq.com>
2024-02-01 16:39:13 +0800

    dev

--- Dmtrack2/settings.py
+++ Dmtrack2/settings.py
@@ -129,9 +129,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Dmtrackdb2',
-        'USER': 'siteusrC',
-        'PASSWORD': 'dm20220418!',
-        'HOST':'192.168.100.10',
+        'USER': 'test',
+        'PASSWORD': 'test',
+        'HOST':'127.0.0.1',
         'PORT':'3306',
         'OPTIONS': {
                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
